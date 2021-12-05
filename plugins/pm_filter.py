8f99ae2c204dc3b863e3176fe686a8d386dd6775
Liku cs <83144124+likucs@users.noreply.github.com>
2021-12-05 10:45:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -810,7 +810,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔖 [{get_size(file.file_size)}] 💌 {file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"🔖 [{get_size(file.file_size)}] ‣ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
